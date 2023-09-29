8dc5f5ef68c1c65c3f8b2dcf363dcb62846f234e
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2023-09-29 13:55:50 +0200

    codegen output 3897b827208f4941b6c7f5f96c46c1e0 (#6)

--- Sources/Schemas/MetadataFilterFiltersField.swift
+++ Sources/Schemas/MetadataFilterFiltersField.swift
@@ -1,7 +1,17 @@
 import Foundation
 
-public class MetadataFilterFiltersField: Codable {
-    public init() {
+public enum MetadataFilterFiltersField: Codable {
+    case metadataFieldFilterString(MetadataFieldFilterString)
+    case metadataFieldFilterFloat(MetadataFieldFilterFloat)
+    case metadataFieldFilterMultiSelect(MetadataFieldFilterMultiSelect)
+    case metadataFieldFilterFloatRange(MetadataFieldFilterFloatRange)
+    case metadataFieldFilterDateRange(MetadataFieldFilterDateRange)
+
+    public init(from decoder: Decoder) throws {
+        throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [], debugDescription: "The rules determining the decoding cannot be established."))
     }
 
+    public func encode(to encoder: Encoder) throws {
+        throw EncodingError.invalidValue(MetadataFilterFiltersField.self, EncodingError.Context(codingPath: [], debugDescription: "The rules determining the encoding cannot be established."))
+    }
 }
