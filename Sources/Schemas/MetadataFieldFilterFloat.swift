d667541ea932b6bb060a8f7b22a7f93bad19063b
box-sdk-build <sdk-build@box.com>
2024-05-08 09:56:40 -0700

    fix: Fix metadata filter resource (box/box-openapi#419)

--- Sources/Schemas/MetadataFieldFilterFloat.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-/// Specifies which `float` field on the template to filter the search
-/// results by.
-public typealias MetadataFieldFilterFloat = [String: Double]
--- Sources/Schemas/MetadataFieldFilterString.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-/// Specifies which text field on the template to filter the search
-/// results by.
-public typealias MetadataFieldFilterString = [String: String]
