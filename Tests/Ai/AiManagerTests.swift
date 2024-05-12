52221e5fd48c7c83de317f4718be55b1d3c8f36f
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-05-12 23:14:32 +0200

    test: Box AI integration tests (box/box-codegen#482) (#91)
    Co-authored-by: Minh Nguyen Cong <clovers1254@gmail.com>

---
+++ Tests/Ai/AiManagerTests.swift
@@ -0,0 +1,37 @@
+import Foundation
+import BoxSdkGen
+import XCTest
+
+class AiManagerTests: XCTestCase {
+    var client: BoxClient!
+
+    override func setUp() async throws {
+        client = CommonsManager().getDefaultClient()
+    }
+
+    public func testAskAiSingleItem() async throws {
+        let fileToAsk: FileFull = try await CommonsManager().uploadNewFile()
+        let response: AiResponse = try await client.ai.createAiAsk(requestBody: AiAsk(mode: AiAskModeField.singleItemQa, prompt: "which direction sun rises", items: [AiAskItemsField(id: fileToAsk.id, type: AiAskItemsTypeField.file, content: "Sun rises in the East")]))
+        XCTAssertTrue(response.answer.contains("East"))
+        XCTAssertTrue(response.completionReason == "done")
+        try await client.files.deleteFileById(fileId: fileToAsk.id)
+    }
+
+    public func testAskAiMultipleItems() async throws {
+        let fileToAsk1: FileFull = try await CommonsManager().uploadNewFile()
+        let fileToAsk2: FileFull = try await CommonsManager().uploadNewFile()
+        let response: AiResponse = try await client.ai.createAiAsk(requestBody: AiAsk(mode: AiAskModeField.multipleItemQa, prompt: "Which direction sun rises?", items: [AiAskItemsField(id: fileToAsk1.id, type: AiAskItemsTypeField.file, content: "Earth goes around the sun"), AiAskItemsField(id: fileToAsk2.id, type: AiAskItemsTypeField.file, content: "Sun rises in the East in the morning")]))
+        XCTAssertTrue(response.answer.contains("East"))
+        XCTAssertTrue(response.completionReason == "done")
+        try await client.files.deleteFileById(fileId: fileToAsk1.id)
+        try await client.files.deleteFileById(fileId: fileToAsk2.id)
+    }
+
+    public func testAiTextGenWithDialogueHistory() async throws {
+        let fileToAsk: FileFull = try await CommonsManager().uploadNewFile()
+        let response: AiResponse = try await client.ai.createAiTextGen(requestBody: AiTextGen(prompt: "Parapharse the document.s", items: [AiTextGenItemsField(id: fileToAsk.id, type: AiTextGenItemsTypeField.file, content: "The Earth goes around the sun. Sun rises in the East in the morning.")], dialogueHistory: [AiTextGenDialogueHistoryField(prompt: "What does the earth go around?", answer: "The sun", createdAt: "2021-01-01T00:00:00Z"), AiTextGenDialogueHistoryField(prompt: "On Earth, where does the sun rise?", answer: "East", createdAt: "2021-01-01T00:00:00Z")]))
+        XCTAssertTrue(response.answer.contains("sun"))
+        XCTAssertTrue(response.completionReason == "done")
+        try await client.files.deleteFileById(fileId: fileToAsk.id)
+    }
+}
