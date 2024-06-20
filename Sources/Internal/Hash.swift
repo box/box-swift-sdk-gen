0846a4189cbe74b9214910bfcbac93b64d1d06af
box-sdk-build <sdk-build@box.com>
2024-06-20 11:11:26 -0700

    feat: Improve chunk upload in Swift (box/box-codegen#515)

--- Sources/Internal/Hash.swift
+++ Sources/Internal/Hash.swift
@@ -11,21 +11,23 @@ public class Hash {
     private static let Base64Encoding = "base64"
 
     private let algorithm: HashName
-    private var data: Data
+    private let sha1: SHA1
+    private var digest: Data?
 
     /// Initializes a `Hash` instance with the specified algorithm.
     ///
     /// - Parameter algorithm: The hashing algorithm to use.
     public init(algorithm: HashName) {
         self.algorithm = algorithm
-        self.data = Data()
+        self.sha1 = SHA1()
+        self.digest = nil
     }
 
     /// Updates the hash with additional data.
     ///
     /// - Parameter data: The data to append to the hash.
     public func updateHash(data: Data) {
-        self.data.append(data)
+        self.sha1.update(data: data)
     }
 
     /// Calculates the digest of the accumulated data using the specified encoding.
@@ -33,11 +35,12 @@ public class Hash {
     /// - Parameter encoding: The string encoding to use for the digest result.
     /// - Returns: The base64-encoded or hexadecimal string representation of the hash digest.
     public func digestHash(encoding: String) async -> String {
-        var digest = Data()
+        if digest == nil {
+            digest = sha1.finalize()
+        }
 
-        switch algorithm {
-        case .sha1:
-            digest = SHA1.sha1(data: data)
+        guard let digest = digest else {
+            return ""
         }
 
         if encoding == Self.Base64Encoding {
