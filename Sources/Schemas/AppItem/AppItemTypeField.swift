04d638a78c8a1f667f23e5c5ba73184b50a8e1de
box-sdk-build <sdk-build@box.com>
2024-09-05 05:44:02 -0700

    docs: Hubs Beta (box/box-openapi#453)

---
+++ Sources/Schemas/AppItem/AppItemTypeField.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum AppItemTypeField: String, CodableStringEnum {
+    case appItem = "app_item"
+}
---
+++ Sources/Schemas/AppItemAssociation/AppItemAssociationTypeField.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum AppItemAssociationTypeField: String, CodableStringEnum {
+    case appItemAssociation = "app_item_association"
+}
