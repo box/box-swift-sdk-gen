40e2279e222df4a88278125a6a9a13ee4605cd69
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-10-24 18:31:43 +0200

    fix: update client error schema to support schema errors (box/box-openapi#467) (#259)

--- Sources/Schemas/ClientError/ClientErrorContextInfoField.swift
+++ Sources/Schemas/ClientError/ClientErrorContextInfoField.swift
@@ -1,29 +1,10 @@
 import Foundation
 
 public class ClientErrorContextInfoField: Codable {
-    private enum CodingKeys: String, CodingKey {
-        case message
-    }
-
-    /// More details on the error.
-    public let message: String?
-
-    /// Initializer for a ClientErrorContextInfoField.
-    ///
-    /// - Parameters:
-    ///   - message: More details on the error.
-    public init(message: String? = nil) {
-        self.message = message
+    public init() {
     }
 
     required public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        message = try container.decodeIfPresent(String.self, forKey: .message)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encodeIfPresent(message, forKey: .message)
     }
 
 }
