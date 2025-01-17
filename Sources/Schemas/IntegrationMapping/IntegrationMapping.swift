cb28e6db015d84b59e6dccf6a2fa90c26de20efa
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-01-17 17:52:09 +0100

    fix: Fix order of fields in the `IntegrationMapping` schema (box/box-openapi#497) (#335)

--- Sources/Schemas/IntegrationMapping/IntegrationMapping.swift
+++ Sources/Schemas/IntegrationMapping/IntegrationMapping.swift
@@ -4,29 +4,23 @@ import Foundation
 /// mapping object.
 public class IntegrationMapping: IntegrationMappingBase {
     private enum CodingKeys: String, CodingKey {
-        case boxItem = "box_item"
         case partnerItem = "partner_item"
-        case createdAt = "created_at"
-        case modifiedAt = "modified_at"
+        case boxItem = "box_item"
         case integrationType = "integration_type"
         case isManuallyCreated = "is_manually_created"
         case options
         case createdBy = "created_by"
         case modifiedBy = "modified_by"
+        case createdAt = "created_at"
+        case modifiedAt = "modified_at"
     }
-
-    /// The Box folder, to which the object from the
-    /// partner app domain (referenced in `partner_item_id`) is mapped
-    public let boxItem: FolderMini
 
     /// Mapped item object for Slack
     public let partnerItem: IntegrationMappingPartnerItemSlackUnion
 
-    /// When the integration mapping object was created
-    public let createdAt: Date?
-
-    /// When the integration mapping object was last modified
-    public let modifiedAt: Date?
+    /// The Box folder, to which the object from the
+    /// partner app domain (referenced in `partner_item_id`) is mapped
+    public let boxItem: FolderMini
 
     /// Identifies the Box partner app,
     /// with which the mapping is associated.
@@ -49,18 +43,22 @@ public class IntegrationMapping: IntegrationMappingBase {
     /// last modified the integration mapping
     public let modifiedBy: UserIntegrationMappings?
 
+    /// When the integration mapping object was created
+    public let createdAt: Date?
+
+    /// When the integration mapping object was last modified
+    public let modifiedAt: Date?
+
     /// Initializer for a IntegrationMapping.
     ///
     /// - Parameters:
     ///   - id: A unique identifier of a folder mapping
     ///     (part of a composite key together
     ///     with `integration_type`)
+    ///   - partnerItem: Mapped item object for Slack
     ///   - boxItem: The Box folder, to which the object from the
     ///     partner app domain (referenced in `partner_item_id`) is mapped
-    ///   - partnerItem: Mapped item object for Slack
     ///   - type: Mapping type
-    ///   - createdAt: When the integration mapping object was created
-    ///   - modifiedAt: When the integration mapping object was last modified
     ///   - integrationType: Identifies the Box partner app,
     ///     with which the mapping is associated.
     ///     Currently only supports Slack.
@@ -73,24 +71,31 @@ public class IntegrationMapping: IntegrationMappingBase {
     ///     created the integration mapping
     ///   - modifiedBy: The user who
     ///     last modified the integration mapping
-    public init(id: String, boxItem: FolderMini, partnerItem: IntegrationMappingPartnerItemSlackUnion, type: IntegrationMappingBaseTypeField = IntegrationMappingBaseTypeField.integrationMapping, createdAt: Date? = nil, modifiedAt: Date? = nil, integrationType: IntegrationMappingIntegrationTypeField? = nil, isManuallyCreated: Bool? = nil, options: IntegrationMappingSlackOptions? = nil, createdBy: UserIntegrationMappings? = nil, modifiedBy: UserIntegrationMappings? = nil) {
-        self.boxItem = boxItem
+    ///   - createdAt: When the integration mapping object was created
+    ///   - modifiedAt: When the integration mapping object was last modified
+    public init(id: String, partnerItem: IntegrationMappingPartnerItemSlackUnion, boxItem: FolderMini, type: IntegrationMappingBaseTypeField = IntegrationMappingBaseTypeField.integrationMapping, integrationType: IntegrationMappingIntegrationTypeField? = nil, isManuallyCreated: Bool? = nil, options: IntegrationMappingSlackOptions? = nil, createdBy: UserIntegrationMappings? = nil, modifiedBy: UserIntegrationMappings? = nil, createdAt: Date? = nil, modifiedAt: Date? = nil) {
         self.partnerItem = partnerItem
-        self.createdAt = createdAt
-        self.modifiedAt = modifiedAt
+        self.boxItem = boxItem
         self.integrationType = integrationType
         self.isManuallyCreated = isManuallyCreated
         self.options = options
         self.createdBy = createdBy
         self.modifiedBy = modifiedBy
+        self.createdAt = createdAt
+        self.modifiedAt = modifiedAt
 
         super.init(id: id, type: type)
     }
 
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        boxItem = try container.decode(FolderMini.self, forKey: .boxItem)
         partnerItem = try container.decode(IntegrationMappingPartnerItemSlackUnion.self, forKey: .partnerItem)
+        boxItem = try container.decode(FolderMini.self, forKey: .boxItem)
+        integrationType = try container.decodeIfPresent(IntegrationMappingIntegrationTypeField.self, forKey: .integrationType)
+        isManuallyCreated = try container.decodeIfPresent(Bool.self, forKey: .isManuallyCreated)
+        options = try container.decodeIfPresent(IntegrationMappingSlackOptions.self, forKey: .options)
+        createdBy = try container.decodeIfPresent(UserIntegrationMappings.self, forKey: .createdBy)
+        modifiedBy = try container.decodeIfPresent(UserIntegrationMappings.self, forKey: .modifiedBy)
         if let _createdAt = try container.decodeIfPresent(String.self, forKey: .createdAt) {
             createdAt = try Utils.Dates.dateTimeFromString(dateTime: _createdAt)
         } else {
@@ -103,19 +108,19 @@ public class IntegrationMapping: IntegrationMappingBase {
             modifiedAt = nil
         }
 
-        integrationType = try container.decodeIfPresent(IntegrationMappingIntegrationTypeField.self, forKey: .integrationType)
-        isManuallyCreated = try container.decodeIfPresent(Bool.self, forKey: .isManuallyCreated)
-        options = try container.decodeIfPresent(IntegrationMappingSlackOptions.self, forKey: .options)
-        createdBy = try container.decodeIfPresent(UserIntegrationMappings.self, forKey: .createdBy)
-        modifiedBy = try container.decodeIfPresent(UserIntegrationMappings.self, forKey: .modifiedBy)
 
         try super.init(from: decoder)
     }
 
     public override func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(boxItem, forKey: .boxItem)
         try container.encode(partnerItem, forKey: .partnerItem)
+        try container.encode(boxItem, forKey: .boxItem)
+        try container.encodeIfPresent(integrationType, forKey: .integrationType)
+        try container.encodeIfPresent(isManuallyCreated, forKey: .isManuallyCreated)
+        try container.encodeIfPresent(options, forKey: .options)
+        try container.encodeIfPresent(createdBy, forKey: .createdBy)
+        try container.encodeIfPresent(modifiedBy, forKey: .modifiedBy)
         if let createdAt = createdAt {
             try container.encode(Utils.Dates.dateTimeToString(dateTime: createdAt), forKey: .createdAt)
         }
@@ -124,11 +129,6 @@ public class IntegrationMapping: IntegrationMappingBase {
             try container.encode(Utils.Dates.dateTimeToString(dateTime: modifiedAt), forKey: .modifiedAt)
         }
 
-        try container.encodeIfPresent(integrationType, forKey: .integrationType)
-        try container.encodeIfPresent(isManuallyCreated, forKey: .isManuallyCreated)
-        try container.encodeIfPresent(options, forKey: .options)
-        try container.encodeIfPresent(createdBy, forKey: .createdBy)
-        try container.encodeIfPresent(modifiedBy, forKey: .modifiedBy)
         try super.encode(to: encoder)
     }
 
