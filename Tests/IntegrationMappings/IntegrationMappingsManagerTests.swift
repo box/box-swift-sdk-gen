d9f3b0fa48e7bbb025982f76bee1bc4515ff0a37
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-04-29 18:29:31 +0200

    test: Improve `Slack` integration mapping test (box/box-codegen#710) (#428)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

--- Tests/IntegrationMappings/IntegrationMappingsManagerTests.swift
+++ Tests/IntegrationMappings/IntegrationMappingsManagerTests.swift
@@ -10,18 +10,32 @@ class IntegrationMappingsManagerTests: XCTestCase {
     }
 
     public func testSlackIntegrationMappings() async throws {
-        let folder: FolderFull = try await client.folders.createFolder(requestBody: CreateFolderRequestBody(name: Utils.getUUID(), parent: CreateFolderRequestBodyParentField(id: "0")))
-        let slackOrgId: String = "1"
-        let partnerItemId: String = "1"
         let userId: String = Utils.getEnvironmentVariable(name: "USER_ID")
+        let slackAutomationUserId: String = Utils.getEnvironmentVariable(name: "SLACK_AUTOMATION_USER_ID")
+        let slackOrgId: String = Utils.getEnvironmentVariable(name: "SLACK_ORG_ID")
+        let slackPartnerItemId: String = Utils.getEnvironmentVariable(name: "SLACK_PARTNER_ITEM_ID")
         let userClient: BoxClient = CommonsManager().getDefaultClientWithUserSubject(userId: userId)
-        await XCTAssertThrowsErrorAsync(try await userClient.integrationMappings.createSlackIntegrationMapping(requestBody: IntegrationMappingSlackCreateRequest(partnerItem: IntegrationMappingPartnerItemSlack(id: partnerItemId, slackOrgId: .value(slackOrgId)), boxItem: IntegrationMappingBoxItemSlack(id: folder.id))))
-        let integrationMappings: IntegrationMappings = try await userClient.integrationMappings.getSlackIntegrationMapping()
-        XCTAssertTrue(integrationMappings.entries!.count == 0)
-        let integrationMappingId: String = "123456"
-        await XCTAssertThrowsErrorAsync(try await userClient.integrationMappings.updateSlackIntegrationMappingById(integrationMappingId: integrationMappingId, requestBody: UpdateSlackIntegrationMappingByIdRequestBody(boxItem: IntegrationMappingBoxItemSlack(id: "1234567"))))
-        await XCTAssertThrowsErrorAsync(try await userClient.integrationMappings.deleteSlackIntegrationMappingById(integrationMappingId: integrationMappingId))
-        try await client.folders.deleteFolderById(folderId: folder.id)
+        let folder: FolderFull = try await userClient.folders.createFolder(requestBody: CreateFolderRequestBody(name: Utils.getUUID(), parent: CreateFolderRequestBodyParentField(id: "0")))
+        try await userClient.userCollaborations.createCollaboration(requestBody: CreateCollaborationRequestBody(item: CreateCollaborationRequestBodyItemField(type: CreateCollaborationRequestBodyItemTypeField.folder, id: folder.id), accessibleBy: CreateCollaborationRequestBodyAccessibleByField(type: CreateCollaborationRequestBodyAccessibleByTypeField.user, id: slackAutomationUserId), role: CreateCollaborationRequestBodyRoleField.coOwner))
+        let slackIntegrations: IntegrationMappings = try await userClient.integrationMappings.getSlackIntegrationMapping()
+        if slackIntegrations.entries!.count == 0 {
+            try await userClient.integrationMappings.createSlackIntegrationMapping(requestBody: IntegrationMappingSlackCreateRequest(partnerItem: IntegrationMappingPartnerItemSlack(id: slackPartnerItemId, slackOrgId: .value(slackOrgId)), boxItem: IntegrationMappingBoxItemSlack(id: folder.id)))
+        }
+
+        let slackMappings: IntegrationMappings = try await userClient.integrationMappings.getSlackIntegrationMapping()
+        XCTAssertTrue(slackMappings.entries!.count >= 1)
+        let slackIntegrationMapping: IntegrationMapping = slackMappings.entries![0]
+        XCTAssertTrue(Utils.Strings.toString(value: slackIntegrationMapping.integrationType) == "slack")
+        XCTAssertTrue(Utils.Strings.toString(value: slackIntegrationMapping.type) == "integration_mapping")
+        XCTAssertTrue(Utils.Strings.toString(value: slackIntegrationMapping.boxItem.type) == "folder")
+        let updatedSlackMapping: IntegrationMapping = try await userClient.integrationMappings.updateSlackIntegrationMappingById(integrationMappingId: slackIntegrationMapping.id, requestBody: UpdateSlackIntegrationMappingByIdRequestBody(boxItem: IntegrationMappingBoxItemSlack(id: folder.id)))
+        XCTAssertTrue(Utils.Strings.toString(value: updatedSlackMapping.boxItem.type) == "folder")
+        XCTAssertTrue(updatedSlackMapping.boxItem.id == folder.id)
+        if slackMappings.entries!.count > 2 {
+            try await userClient.integrationMappings.deleteSlackIntegrationMappingById(integrationMappingId: slackIntegrationMapping.id)
+        }
+
+        try await userClient.folders.deleteFolderById(folderId: folder.id)
     }
 
     public func testTeamsIntegrationMappings() async throws {
